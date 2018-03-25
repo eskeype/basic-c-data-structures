fcd06363e070ae573e824acc187ca675e18142d4
eskeype <sameer@SKPs-MacBook-Air.local>
2018-03-25 19:41:20 -0400

    Updated test_vector

--- test/Lists/test_vector.c
+++ test/Lists/test_vector.c
@@ -12,12 +12,44 @@ static char * test_append(){
 	vector_append(v, "one");
 	vector_append(v, "two");
 
-	mu_assert("vector_get(0) is not 'zero'", strcmp((char*) vector_get(v, 0), "zero") == 0);
-	mu_assert("vector_get(1) is not 'one'", strcmp((char*) vector_get(v, 1), "one") == 0);
-	mu_assert("vector_get(2) is not 'two'", strcmp((char*) vector_get(v, 2), "two") == 0);
+	mu_assert("vector_length(v) is not 3", vector_length(v) == 3);
+
+	mu_assert("vector_get(v, 0) is not 'zero'", strcmp((char*) vector_get(v, 0), "zero") == 0);
+	mu_assert("vector_get(v, 1) is not 'one'", strcmp((char*) vector_get(v, 1), "one") == 0);
+	mu_assert("vector_get(v, 2) is not 'two'", strcmp((char*) vector_get(v, 2), "two") == 0);
 
 	return 0;
 }
+
+static char * test_remove(){
+	vector * v = vector_new();
+
+	vector_append(v, "zero");
+	vector_append(v, "one");
+	vector_append(v, "two");
+	vector_append(v, "three");
+	vector_append(v, "four");
+	vector_append(v, "five");
+	vector_append(v, "six");
+	vector_append(v, "seven");
+	vector_append(v, "eight");
+	vector_append(v, "nine");
+	
+	vector_remove(v, 0);
+
+	mu_assert("vector_get(v, 0) is not 'one'", strcmp((char*) vector_get(v, 0), "one") == 0);
+
+	for(int i = 0; i < 8; i++){
+		vector_remove(v, 0);
+	}
+
+	mu_assert("vector_get(v, 0) is not 'nine'", strcmp((char*) vector_get(v, 0), "nine") == 0);
+
+	return 0;
+
+}
+
+//test the space usage
 
 static char * all_tests() {
     mu_run_test(test_append);
